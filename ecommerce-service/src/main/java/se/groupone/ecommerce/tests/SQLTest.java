d47b47a593166e4d4d4f9764e0962428d03624df
Erik Welander <erik.welander@hotmail.com>
2015-03-02 15:17:40 +0100

    + SQL fixes and testing

--- ecommerce-service/src/main/java/se/groupone/ecommerce/tests/SQLTest.java
+++ ecommerce-service/src/main/java/se/groupone/ecommerce/tests/SQLTest.java
@@ -1,4 +1,7 @@
 package se.groupone.ecommerce.tests;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 import se.groupone.ecommerce.repository.sql.SQLConnector;
@@ -17,6 +20,25 @@ public class SQLTest
 	{
 		SQLConnector sql = new SQLConnector(host, port, username, password, database);
 		assertTrue(sql.connect());
+		assertTrue(sql.disconnect());
+	}
+	
+	@Test
+	public void addRemove() throws SQLException
+	{
+		SQLConnector sql = new SQLConnector(host, port, username, password, database);
+		assertTrue(sql.connect());
+		
+		String queryInsert = "INSERT INTO customer(user_name, password, email, first_name, last_name, address, phone)"
+				+ " VALUES('kira', 'Elf', 'erik.welander@hotmail.com', 'Erik', 'Welander', 'Terserusvägen 12', '073');";
+		
+		String queryResult = "SELECT user_name FROM ecomm.customer;";
+		
+		assertTrue(sql.queryInsert(queryInsert));
+		ResultSet rs = sql.queryResult(queryResult);
+		rs.next();
+		assertEquals("kira", rs.getString("user_name"));
+		
 		assertTrue(sql.disconnect());
 	}
 	
