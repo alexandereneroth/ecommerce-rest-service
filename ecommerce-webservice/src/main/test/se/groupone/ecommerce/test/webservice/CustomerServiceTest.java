58c1b767d522a4131f80e497dd4730aca86fca2a
alexander-eneroth <89alexius@gmail.com>
2015-03-02 15:29:29 +0100

    + adds the test source folder. + adds the CustomerServiceTest file.

---
+++ ecommerce-webservice/src/main/test/se/groupone/ecommerce/test/webservice/CustomerServiceTest.java
@@ -0,0 +1,51 @@
+package se.groupone.ecommerce.test.webservice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// Uppdatera en användare 
+//
+// Ta bort en användare (eller sätta den som inaktiv)
+//
+// Skapa en order för en användare
+//
+// Hämta en användares alla order
+//
+// Hämta en viss order för en användare 
+//
+// Uppdatera en order för en användare 
+//
+// Ta bort en order för en användare
+
+public class CustomerServiceTest
+{
+	// Hämta en användare med ett visst id
+	@Test
+	public void canGetCustomerOfId()
+	{
+		assertTrue("unimplemented", false); // TODO
+	}
+	
+	// Skapa en ny användare
+	@Test
+	public void canCreateCustomer()
+	{
+		assertTrue("unimplemented", false); // TODO
+	}
+
+	// Skapa en ny användare – detta ska returnera en länk till den skapade 
+	//användaren i Location-headern
+	@Test
+	public void assertLocationHeaderOfCreatedCustomerIsCorrect()
+	{
+		assertTrue("unimplemented", false); // TODO
+	}
+	
+	// Uppdatera en användare 
+	@Test
+	public void canUpdateUser()
+	{
+		assertTrue("unimplemented", false); // TODO
+	}
+}
