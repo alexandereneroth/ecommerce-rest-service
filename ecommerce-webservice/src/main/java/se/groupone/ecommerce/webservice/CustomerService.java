34c83fc18dd34bb60c27087270666f1c08f6080f
alexander-eneroth <89alexius@gmail.com>
2015-03-02 14:55:14 +0100

    + adds the customer requirements from the product description to CustomerService.java

--- ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/CustomerService.java
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/CustomerService.java
@@ -1,6 +1,7 @@
 package se.groupone.ecommerce.webservice;
 
 import javax.ws.rs.DELETE;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -8,9 +9,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+// Hämta en användare med ett visst id
+//
+// Skapa en ny användare – detta ska returnera en länk till den skapade 
+//
+//användaren i Location-headern
+//
+// Uppdatera en användare 
+//
+// Ta bort en användare (eller sätta den som inaktiv)
+//
+// Skapa en order för en användare
+//
+// Hämta en användares alla order
+//
+// Hämta en viss order för en användare 
+//
+// Uppdatera en order för en användare 
+//
+// Ta bort en order för en användare
+
 @Path("customer")
 public class CustomerService
 {
+
 	@GET
 	public Response getAllCustomers()
 	{
