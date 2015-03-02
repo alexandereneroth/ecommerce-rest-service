62a1318eefc5dce2c323d727f685c09ee1a5530e
alexander-eneroth <89alexius@gmail.com>
2015-03-02 15:09:01 +0100

    + added junit,hamcrest,mockito to pom file dependencies.

---
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/test/webservice/CustomerServiceTest.java
@@ -0,0 +1,16 @@
+package se.groupone.ecommerce.test.webservice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerServiceTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
