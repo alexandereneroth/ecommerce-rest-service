ef2cf1a0370c7bf6ca49a175672f2a0d42f8703e
Jitan Elis <jitan.elis@gmail.com>
2015-03-06 12:55:50 +0100

    + Changes getOrders() in ShopService to only return orders for one specific customer, also implements this in InMemory repo

--- ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/OrderService.java
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/OrderService.java
@@ -2,23 +2,51 @@ package se.groupone.ecommerce.webservice;
 
 import se.groupone.ecommerce.model.Order;
 
+import java.util.ArrayList;
+
 import se.groupone.ecommerce.exception.ShopServiceException;
 import se.groupone.ecommerce.service.ShopService;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-@Path("customer")
+@Path("order")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public final class OrderService
 {
 	@Context
 	private ServletContext context;
 	private static ShopService ss;
+	
+	@GET
+	@Path("{username}")
+	public Response getOrder(@PathParam("username") final String username )
+	{
+		ShopService ss = (ShopService) context.getAttribute("ss");
+		ArrayList<Order> orderList;
+		try
+		{
+			orderList = new ArrayList<Order>(ss.getOrders());
+			StringBuilder builder = new StringBuilder();
+			for (Order order : orderList) {
+				builder.append(order.toString());
+			}
+			return Response.ok(builder.toString()).build();
+		}
+		catch (ShopServiceException e)
+		{
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+	}
 	
 	@GET
 	@Path("{orderId}")
