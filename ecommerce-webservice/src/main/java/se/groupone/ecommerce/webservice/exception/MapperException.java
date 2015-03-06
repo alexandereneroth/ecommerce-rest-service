066f34c9b3ff0e4bb4f9697a673e4a9d9cac467c
Jitan Elis <jitan.elis@gmail.com>
2015-03-07 00:37:48 +0100

    Added TODO on MapperException

--- ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/exception/MapperException.java
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/exception/MapperException.java
@@ -1,5 +1,6 @@
 package se.groupone.ecommerce.webservice.exception;
 
+//TODO Not used right now.. maybe later
 public class MapperException extends Exception
 {
 	private static final long serialVersionUID = 13123L;
