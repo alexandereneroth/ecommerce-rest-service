e7d446a71a3502760a60ef8e220da6594101dbc6
alexander-eneroth <89alexius@gmail.com>
2015-03-09 16:53:43 +0100

    + adds a reader to ProductListMapper.

--- ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/util/ProductListMapper.java
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/util/ProductListMapper.java
@@ -1,8 +1,11 @@
 package se.groupone.ecommerce.webservice.util;
 
 import se.groupone.ecommerce.model.Product;
+import se.groupone.ecommerce.model.ProductParameters;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
@@ -14,13 +17,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
@@ -30,7 +37,7 @@ import com.google.gson.stream.JsonWriter;
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public final class ProductListMapper implements MessageBodyWriter<ArrayList<Product>>
+public final class ProductListMapper implements MessageBodyWriter<ArrayList<Product>>, MessageBodyReader<ArrayList<Product>>
 {
 	private Gson gson;
 	private Type productListType = new TypeToken<ArrayList<Product>>()
@@ -66,7 +73,21 @@ public final class ProductListMapper implements MessageBodyWriter<ArrayList<Prod
 		}
 	}
 
-	private static final class ProductAdapter implements JsonSerializer<ArrayList<Product>>
+	@Override
+	public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+	{
+		return genericType.equals(productListType);
+	}
+
+	@Override
+	public ArrayList<Product> readFrom(Class<ArrayList<Product>> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+			MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException
+	{
+		final ArrayList<Product> productArrayList = gson.fromJson(new InputStreamReader(entityStream), productListType);
+		return productArrayList;
+	}
+
+	private static final class ProductAdapter implements JsonSerializer<ArrayList<Product>>, JsonDeserializer<ArrayList<Product>>
 	{
 		@Override
 		public JsonElement serialize(ArrayList<Product> productList, Type typeOfSrc, JsonSerializationContext context)
@@ -88,6 +109,33 @@ public final class ProductListMapper implements MessageBodyWriter<ArrayList<Prod
 				productListJson.add("" + product.getId(), productJson);
 			}
 			return productListJson;
+		}
+		@Override
+		public ArrayList<Product> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+		{
+			final JsonObject productListJson = json.getAsJsonObject();
+			
+			final ArrayList<Product> products = new ArrayList<>();
+			
+			final int NUMBER_OF_PRODUCT_PARAMS = 8;
+			
+			for(int i = 1; i < NUMBER_OF_PRODUCT_PARAMS; ++i)
+			{
+				JsonObject productJson = (JsonObject) productListJson.get(String.valueOf(i));
+
+				final int id = productJson.get("id").getAsInt();
+				ProductParameters params = new ProductParameters(
+						productJson.get("title").getAsString(),
+						productJson.get("category").getAsString(),
+						productJson.get("manufacturer").getAsString(),
+						productJson.get("description").getAsString(),
+						productJson.get("img").getAsString(),
+						productJson.get("price").getAsDouble(),
+						productJson.get("quantity").getAsInt());
+				
+				products.add(new Product(id, params));
+			}
+			return products;
 		}
 	}
 }
