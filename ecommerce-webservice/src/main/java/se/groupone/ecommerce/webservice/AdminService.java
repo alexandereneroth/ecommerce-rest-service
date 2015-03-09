2a28e59abd0751df9709296c2231f43dee898ca3
Jitan Elis <jitan.elis@gmail.com>
2015-03-09 13:53:55 +0100

    Adds AdminService resource which enables resetting of InMemory repo through API call

---
+++ ecommerce-webservice/src/main/java/se/groupone/ecommerce/webservice/AdminService.java
@@ -0,0 +1,33 @@
+package se.groupone.ecommerce.webservice;
+
+import se.groupone.ecommerce.repository.memory.InMemoryCustomerRepository;
+import se.groupone.ecommerce.repository.memory.InMemoryOrderRepository;
+import se.groupone.ecommerce.repository.memory.InMemoryProductRepository;
+import se.groupone.ecommerce.service.ShopService;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+@Path("admin")
+public class AdminService
+{
+	@Context
+	private ServletContext context;
+	
+	@POST
+	public Response readAdminCommand(String command) 
+	{
+		if(command.equals("reset-repo"))
+		{
+			ShopService newShopService = new ShopService(new InMemoryCustomerRepository(), 
+					new InMemoryProductRepository(), new InMemoryOrderRepository());
+			
+			context.setAttribute("ss", newShopService);
+			return Response.ok("InMemoryRepo has been reset").build();
+		}
+		return Response.status(400).entity("Invalid command received").build();
+	}
+}
